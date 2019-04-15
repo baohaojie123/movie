6636a2eada736f358caeb901bca4f4a9c65f941a
baohaojie <baohaojie@founq.com>
2019-04-15 19:28:20 +0800

    post

---
+++ pages/posts/post-detail/post-detail.js
@@ -0,0 +1,202 @@
+// pages/posts/post-detail/post-detail.js
+var postsData = require('../../../data/posts-data.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isPlayingMusic:false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (option) {
+    var collected;
+    // 接收传参
+    var postId = option.id;
+    this.data.currentPostId = postId;
+    var postData = postsData.postList[postId];
+    this.setData({
+      postData: postData
+      // posts_content: postsData.local_database
+    });
+    // 设置同步缓存 永久有效 缓存的上限最大不能超过10M
+    // wx.setStorageSync('key', {
+    //   game:"风暴英雄",
+    //   developer:"暴雪"
+    // });
+    // wx.setStorageSync('key1', {
+    //   game:'lol',
+    //   developer:'拳头'
+    // })
+
+    // 假设一下数据结构为收藏与否的状态
+  // var postsCollected = {
+  //   0:"true",
+  //   2:'false',
+  //   3:'true'
+  //   ...
+  // }
+    var postsCollected = wx.getStorageSync('posts_Collected');
+    if (postsCollected){
+      var postCollected = postsCollected[postId];
+      if (postCollected) {
+        this.setData({
+          collected: postCollected
+        })
+      }
+    }else{
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_Collected', postsCollected);
+    }
+    var that = this
+    wx.onBackgroundAudioPause(function(){
+      that.setData({
+        isPlayingMusic:false
+      })
+    })
+    wx.onBackgroundAudioPlay(function(){
+      that.setData({
+        isPlayingMusic:true
+      })
+    })
+
+  },
+  onCollectionTap:function(event){
+    // var game = wx.getStorageSync('key');
+    // console.log(game);
+    var postsCollected = wx.getStorageSync('posts_Collected');
+    var postCollected = postsCollected[this.data.currentPostId];
+    // 收藏变成未收藏，未收藏变成收藏
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    this.showModal(postsCollected, postCollected);
+  },
+  showToast: function (postsCollected, postCollected){
+    // 更新文章是否被收藏的缓存值
+    wx.setStorageSync('posts_Collected', postsCollected);
+    // 更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected: postCollected
+    })
+    wx,wx.showToast({
+      title: postCollected ? '收藏成功' : ' 取消成功',
+      icon: 'success',
+      duration: 1000
+    })
+  },
+  onMusicTap: function (event) {
+    var isPlayingMusic = this.data.isPlayingMusic;
+    var currentPostId = this.data.currentPostId;
+    var postData = postsData.postList[currentPostId];
+    if (!isPlayingMusic){
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg
+      });
+      this.setData({
+        isPlayingMusic:true
+      })
+    }else {
+      wx.pauseBackgroundAudio()
+      this.setData({
+        isPlayingMusic:false
+      })
+    }
+  },
+  showModal: function (postsCollected, postCollected){
+    var that  = this;
+    wx.showModal({
+      title: '收藏',
+      content: postCollected ? '是否收藏该文章？' :'取消收藏该文章？',
+      showCancel: 'true',
+      cancelText: '取消',
+      cancelColor: '#333',
+      confirmText: '确认',
+      confrimColor: '#405f80',
+      success:function(res){
+        if(res.confirm){
+          // 更新文章是否被收藏的缓存值
+          wx.setStorageSync('posts_Collected', postsCollected);
+          that.showToast(postsCollected, postCollected);
+          // 更新数据绑定变量，从而实现切换图片
+          that.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  },
+
+  onShareTap:function(event){
+    // wx.removeStorageSync('key');
+    // 清除所有缓存
+    // wx.clearStorageSync();
+    var itemList = ['分享给微信好友', '分享到朋友圈', '分享到QQ', '分享到微博'];
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor:'#405f80',
+      success:function(res){
+        // res.cancel 用户是否点击了取消按钮
+        // res.tapIndex 数组元素的序号，从0开始
+        wx.showModal({
+          title: '用户'+itemList[res.tapIndex],
+          content: '用户是否取消？'+res.cancel+"现在无法实现分享功能",
+        })
+      }
+
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/posts/posts.js
@@ -0,0 +1,104 @@
+// pages/posts/posts.js
+// 只能用相对路径
+var postsData = require('../../data/posts-data.js')
+Page({
+  // 处理用户交互逻辑
+  // 产生事件-》捕捉事件（回调函数（处理事件））
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 单向数据绑定，页面中的值不能  自动  改变js中的值
+    date: 'Nov 20 2016',
+    title: '正是虾肥蟹壮时'
+
+  },
+
+  /**
+   * 1 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // var posts_content = [{
+    //   date: "Sep 18 2016",
+    //   title: "正是虾肥蟹壮时",
+    //   imgSrc: "/images/post/crab.png",
+    //   avatar: "/images/avatar/1.png",
+    //   content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+    //   reading: "112",
+    //   collection: "96",
+    // }, {
+    //   title: "比利·林恩的中场故事",
+    //   content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+    //   imgSrc: "/images/post/bl.png",
+    //   reading: 62,
+    //   collection: 92,
+    //   date: "Nov 20 2016",
+    //   avatar: "/images/avatar/1.png",
+    // }]
+    // 给数组一个整体的名字，因为posts_content是一个数组，我们setData必须是一个对象，所以此处需要转化。如果posts_content是一个对象直接 this.setData(posts_content);
+    // this.setData({
+    //   posts_key:posts_content
+    //   });
+    // es6 简写
+    this.setData({
+      posts_content: postsData.postList
+      // posts_content: postsData.local_database
+    });
+  },
+  onPostTap:function(event){
+    
+    // dataset 所有自定义属性的集合
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id='+postId
+    })
+  },
+  /*i
+   * 3 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   *2  生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
